ad5dfef16226b7584a71ae199a55464ff8522950
Jay Bhat <jbhat@ooyala.com>
2012-04-02 18:02:59 -0700

    begin implementation of insert and remove

--- lib/cassandra-queue.rb
+++ lib/cassandra-queue.rb
@@ -4,6 +4,10 @@
 require "cassandra"
 include SimpleUUID
 
+DEFAULT_KEYSPACE = "QueueInfo"
+DEFAULT_SERVERS = ["127.0.0.1:9160"]
+DEFAULT_QUEUE_CF = :Queue
+
 module CassandraQueue
   # Singleton class that manages our cassandra queues
   class QueueManager
@@ -19,7 +23,7 @@ module CassandraQueue
 
   class Queue
     # Entry point for using a queue.  Class method which will return you a queue object for that UUID
-    def self.get_queue(qid, keyspace = nil, servers = nil)
+    def self.get_queue(qid, keyspace = DEFAULT_KEYSPACE, servers = DEFAULT_SERVERS)
       QueueManager.queue(qid, keyspace, servers)
     end
 
@@ -27,23 +31,30 @@ module CassandraQueue
       # Fail if called directly.  We want queues to be managed by QueueManager
       raise "Please create a managed queue using Queue::get_queue" unless caller[1] =~ /in `queue'/
 
-      @key = qid
+      @key = qid_to_rowkey qid
       # Set cassandra client if it has not already been set
       @client ||= create_client(keyspace, servers)
+      @queue_cf = DEFAULT_QUEUE_CF
+    end
+
+    def qid_to_rowkey(qid)
+      qid
     end
 
     def create_client(keyspace, servers)
       ::Cassandra.new(keyspace, servers.flatten)
     end
 
-    # Takes a message, throws it on the queue, and returns the message with a new part for the TimeUUID
+    # Takes a payload, throws it on the queue, and returns the TimeUUID that was created for it
     def insert(payload, time=Time.now)
-
+      timeUUID = UUID.new(time)
+      @client.insert(@queue_cf, @key, timeUUID => payload)
+      timeUUID
     end
 
     # Removes a TimeUUID, and it's message, from the queue
     def delete(timeUUID)
-
+      @client.remove(@queue_cf, @key, timeUUID)
     end
 
     # Show the current state of the queue, for things such as failure recovery
